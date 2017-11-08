b8885ae4cffd588ed2917877e5ea52352ffa5526
Hui Zhou <hzhou@hzsolutions.net>
2017-11-07 20:10:36 -0600

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ ln -s $HOME/projects/my_configs/_bashrc $HOME/.bashrc
 ln -s $HOME/projects/my_configs/_vimrc  $HOME/.vimrc
 ln -s $HOME/projects/my_configs/_vim  $HOME/.vim
 
-source $HOME/.bashrc
-
 cd $HOME/projects
 git clone https://github.com/hzhou/MyDef
 cd MyDef
 sh bootstrap.sh
+
+source $HOME/.bashrc
 
 for a in output_c output_java output_python output_tcl output_glsl; do
     cd $HOME/projects
