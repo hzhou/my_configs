86b6c01a5e3c754fabcfed3f5552def0526cfbc6
Hui Zhou <hzhou@hzsolutions.net>
2017-11-13 12:46:38 -0600

    modified:   _bashrc 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,43 @@
-ln -s $HOME/projects/my_configs/_bashrc $HOME/.bashrc
-ln -s $HOME/projects/my_configs/_vimrc  $HOME/.vimrc
-ln -s $HOME/projects/my_configs/_vim  $HOME/.vim
+cp -v _bashrc $HOME/.bashrc
+cp -v _vimrc  $HOME/.vimrc
+cp -rv _vim $HOME/.vim
 
 cd $HOME/projects
-git clone https://github.com/hzhou/MyDef
-cd MyDef
+if [ -d MyDef ]; then
+    cd MyDef
+    git pull
+else
+    git clone https://github.com/hzhou/MyDef
+    cd MyDef
+fi
 sh bootstrap.sh
 
-source $HOME/.bashrc
+PERL5LIB=$HOME/lib/perl5
+MYDEFLIB=$HOME/lib/MyDef
+MYDEFSRC=$HOME/projects/MyDef
+
+PATH=$HOME/bin:$PATH
 
 for a in output_c output_java output_python output_tcl output_glsl; do
     cd $HOME/projects
-    git clone https://github.com/hzhou/$a
-    cd $a
+    if [ -d $a ]; then
+        cd $a
+        git pull
+    else
+        git clone https://github.com/hzhou/$a
+        cd $a
+    fi
     mydef_make
     make
     make install
 done
 
 cd $HOME/projects
-git clone https://github.com/hzhou/teach_2017
+if [ -d teach_2017 ]; then
+    cd teach_2017
+    git pull
+else
+    git clone https://github.com/hzhou/teach_2017
+fi
+
+echo "RUN . ~/.bashrc"
